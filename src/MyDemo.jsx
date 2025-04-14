98f2e722470267df8f2e3b3df25ca7db9e0f03d6
youcantfindandrew <108627681+youcantfindandrew@users.noreply.github.com>
2025-04-14 09:05:12 -0700

    Update MyDemo.jsx

--- src/MyDemo.jsx
+++ src/MyDemo.jsx
@@ -1,11 +1,26 @@
+import React, {use, useState} from "react";
+
+
 function MyDemo(){
 
+    const [name, setName] = useState("enter name above")
+    const [answer, setAnswer] = useState("");
+    const handleNameInput = () => (
+        setName(document.getElementById("myName").value)
+    )
     return(
-<ul>
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Contact</a></li>
-</ul>
+        <div>
+            <input id="myName" placeholder="Name.."></input>
+            <button onClick={handleNameInput}>Submit name</button>
+            <p>Name:{name}</p>
+            <p>Answer: {answer} </p>
+            <label>  
+            <input name = "rb" type="radio" value ="Yes" checked = {answer == "Yes"}
+            onChange={(e)=> setAnswer(e.target.value)}/> Yes 
+            <input name = "rb" type="radio" value ="No" checked = {answer == "No"}
+            onChange={(e)=> setAnswer(e.target.value)}/> No
+            </label>
+        </div>
     );
 }
 export default MyDemo
