f265993f9f371172c25e89b6b94a81f178f64598
Andrew Lwin <andrewlwin100@gmail.com>
2025-04-14 09:53:05 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactApp/', //Replace with your GitHub repo name
+  base: '/React-App/', //Replace with your GitHub repo name
 });
